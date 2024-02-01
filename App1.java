ea72a2d84f052554a90083264643962ca20f1b44
curiousrishabh <rishabhbaghel80@gmail.com>
2024-02-01 15:44:34 +0530

    STAQTASK

--- App1.java
+++ App1.java
@@ -3,6 +3,8 @@ public class App1{
         System.out.println("Java App Pulled & Build by Jenkins");
         System.out.println("Testing 2nd times");
         System.out.println("Hello Jenkins");
+        System.out.println("3rd changes");
+        System.out.println("Staq task done");
     }
 
 }
